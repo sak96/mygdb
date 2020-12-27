cae872208da5bf2d98721dd6102e3807516f04fe
sak96 <26397224+sak96@users.noreply.github.com>
2020-12-27 19:11:26 +0530

    Add run command

--- src/debugger.rs
+++ src/debugger.rs
@@ -2,9 +2,10 @@ extern crate structopt;
 
 use interpreter::Interpreter;
 use structopt::{clap::AppSettings, StructOpt};
+use target::Target;
 
 pub struct Debugger {
-    binary: String,
+    target: Target,
     interpreter: Interpreter,
 }
 
@@ -20,7 +21,7 @@ enum DebugCommand {
 impl Debugger {
     pub fn new(binary: &str) -> Self {
         Self {
-            binary: binary.to_string(),
+            target: Target::new(binary),
             interpreter: Interpreter::new(&format!("{}> ", env!("CARGO_PKG_NAME"))),
         }
     }
@@ -29,15 +30,73 @@ impl Debugger {
             match DebugCommand::from_iter_safe(line.split_whitespace()) {
                 Ok(cmd) => match cmd {
                     DebugCommand::Quit => break,
-                    DebugCommand::Run { args } => {
-                        println!("run {} with args {:?}", self.binary, args)
-                    }
+                    DebugCommand::Run { args } => self.target.run(&args),
                 },
                 Err(_) => DebugCommand::clap()
                     .after_help("")
                     .print_long_help()
                     .unwrap_or(()),
             };
+        }
+    }
+}
+
+pub(super) mod target {
+    use std::process::Child;
+    use std::process::Command;
+
+    use nix::{sys::wait, unistd::Pid};
+
+    pub struct Target {
+        binary: String,
+        process: Option<Child>,
+    }
+
+    impl Drop for Target {
+        fn drop(&mut self) {
+            self.kill()
+        }
+    }
+
+    impl Target {
+        pub fn new(binary: &str) -> Self {
+            Self {
+                binary: binary.to_string(),
+                process: None,
+            }
+        }
+
+        pub fn kill(&mut self) {
+            if let Some(mut process) = self.process.take() {
+                process.kill().unwrap_or(())
+            }
+        }
+
+        pub fn pid(&self) -> Option<Pid> {
+            if let Some(ref process) = self.process {
+                Some(Pid::from_raw(process.id() as i32))
+            } else {
+                None
+            }
+        }
+
+        pub fn run(&mut self, args: &Vec<String>) {
+            self.kill();
+
+            let mut cmd = Command::new(&self.binary);
+            cmd.args(args);
+            self.process = match cmd.spawn() {
+                Ok(process) => Some(process),
+                Err(err) => {
+                    eprintln!("Error: could not start process\n{}", err);
+                    None
+                }
+            };
+            if let Some(pid) = self.pid() {
+                match wait::waitpid(pid, None) {
+                    some => println!("Wait Status {:?}", some),
+                }
+            }
         }
     }
 }
