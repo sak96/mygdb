2c79cd292ce4a988b62aefbde669dc5652234764
sak96 <26397224+sak96@users.noreply.github.com>
2020-12-27 13:26:07 +0530

    Add interpreter

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,14 +1,49 @@
+use self::interpreter::Interpreter;
+
 pub struct Debugger {
     binary: String,
+    interpreter: Interpreter,
 }
 
 impl Debugger {
     pub fn new(binary: &str) -> Self {
         Self {
             binary: binary.to_string(),
+            interpreter: Interpreter::new(&format!("{}> ", env!("CARGO_PKG_NAME"))),
         }
     }
     pub fn run(&mut self) {
-        println!("debugging the program {}", self.binary);
+        while let Ok(line) = self.interpreter.read_line() {
+            println!("perform '{}' on '{}'", line, self.binary);
+        }
+    }
+}
+
+pub(super) mod interpreter {
+    use rustyline::{error::ReadlineError, Editor};
+
+    pub struct Interpreter {
+        prompt: String,
+        reader: Editor<()>,
+    }
+
+    impl Interpreter {
+        pub fn new(prompt: &str) -> Self {
+            let reader = Editor::<()>::new();
+            Self {
+                prompt: prompt.to_string(),
+                reader,
+            }
+        }
+
+        pub fn read_line(&mut self) -> Result<String, ReadlineError> {
+            match self.reader.readline(self.prompt.as_str()) {
+                Ok(line) => {
+                    self.reader.add_history_entry(&line);
+                    Ok(line)
+                }
+                err => err,
+            }
+        }
     }
 }
